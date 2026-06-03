09286fc183c14f99ed94aefe8530d13dba068aca
KingShadP <kshadp@gmail.com>
2026-06-03 01:42:29 -1000

    sd

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "KingShadP | The Living Archive";
+export const SITE_TITLE = "KingShadP | The Investment";
 export const SITE_DESCRIPTION =
-	"The official digital home for KingShadP: music, visuals, writing, worldbuilding, and creative proof.";
+	"The Multiverse for KingShadP: music, visuals, writing, worldbuilding, and creative proof.";
